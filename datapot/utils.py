954839a2309f72323297aa761f808ab7e23e64ba
YuryMokriy <mavrandr@mail.ru>
2017-02-26 20:14:39 +0300

    Rewrite csv_to_jsonlines function + add notebook example for utils module functions

--- datapot/utils.py
+++ datapot/utils.py
@@ -2,20 +2,11 @@ import re
 
 import pandas as pd
 
-def csv_to_jsonlines(input_file, output_file, **pd_read_csv_options):
-    close_input_file = close_output_file = False
-    if isinstance(input_file, str):
-        input_file = open(input_file, 'r')
-        close_input_file = True
-    if isinstance(output_file, str):
-        output_file = open(output_file, 'w')
-        close_output_file = True
 
-    df = pd.read_csv(input_file, **pd_read_csv_options)
-    json_string = df.to_json(orient='records')[1:-1]
-    jsonlines_string = re.sub('},', '}\n', json_string)
-    output_file.write(jsonlines_string)
-    if close_input_file:
-        input_file.close()
-    if close_output_file:
-        output_file.close()
+def csv_to_jsonlines(csv_file, jsonlines_file, **pd_read_csv_options):
+    with open(csv_file, 'r') if isinstance(csv_file, str) else csv_file as csv_file, \
+         open(jsonlines_file, 'w') if isinstance(jsonlines_file, str) else jsonlines_file as jsonlines_file:
+        df = pd.read_csv(csv_file, **pd_read_csv_options)
+        json_string = df.to_json(orient='records')[1:-1]
+        jsonlines_string = re.sub('},', '}\n', json_string)
+        jsonlines_file.write(jsonlines_string)
