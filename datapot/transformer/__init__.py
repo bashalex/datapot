66cafcc009f9293127d3961a4b6a85a93ef3b3a1
YuryMokriy <mavrandr@mail.ru>
2017-03-03 17:41:02 +0300

    Make small changes

--- datapot/transformer/__init__.py
+++ datapot/transformer/__init__.py
@@ -3,7 +3,6 @@ from .boolean_transformer import TestBoolTransformer
 from .complex_transformer import TestComplexTransformer
 from .text_transformer import TfidfTransformer
 from .timeseries_transformer import TimeSeriesTransformer
-# from .text.text_transformer import Word2VecTransformer # It doesn't work now
 from .timestamp_transformer import TimestampTransformer
 from .category_transformer import SVDOneHotTransformer
 
