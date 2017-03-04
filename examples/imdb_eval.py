5d45c0cdcf282e2ebeffee0479e1d27fef5f4dbe
Savelyev Nikita <n.a.savelyev@gmail.com>
2017-03-04 15:40:33 +0300

    add examples to CI

--- examples/imdb_eval.py
+++ examples/imdb_eval.py
@@ -5,6 +5,7 @@ Usage example for unstructured textual bzip2-compressed data
 datapot.fit method subsamples the data to detect language and choose corresponding stopwords and stemming.
 For each review datapot.transform generates an SVD-compressed 12-dimensional tfidf-vector representation.
 '''
+from __future__ import print_function
 
 import sys
 import bz2
@@ -14,27 +15,30 @@ from sklearn.model_selection import cross_val_score
 import datapot as dp
 from datapot.datasets import fetch_imdb
 
-def main():
-    fetch_imdb()
-    data = bz2.BZ2File('data/imdb.jsonlines.bz2')
+fetch_imdb()
+data = bz2.BZ2File('data/imdb.jsonlines.bz2')
 
-    datapot = dp.DataPot()
-    t0 = time.time()
-    datapot.fit(data)
-    print('fit time:', time.time()-t0)
+datapot = dp.DataPot()
+t0 = time.time()
+datapot.fit(data)
+print('fit time:', time.time()-t0)
+datapot.remove_transformer('sentiment', 0)
 
-    t0 = time.time()
-    df = datapot.transform(data, verbose=True)
-    print('transform time:', time.time()-t0)
+t0 = time.time()
+df = datapot.transform(data, verbose=True)
+print('transform time:', time.time()-t0)
 
-    X = df.drop(['sentiment', 'id'], axis=1)
-    y = df['sentiment']
-    model = xgb.XGBClassifier()
-    print('Cross-val score:', cross_val_score(model, X, y, cv=5))
+X = df.drop(['sentiment', 'id'], axis=1)
+y = df['sentiment']
+model = xgb.XGBClassifier()
+cv_score = cross_val_score(model, X, y, cv=5)
+assert all(i > 0.5 for i in cv_score)
 
-    model.fit(X, y)
-    print('Feature importance:')
-    print(list(zip(X.columns, model.feature_importances_)))
+print('Cross-val score:', cv_score)
 
-if __name__ == "__main__":
-    main()
+model.fit(X, y)
+fi = model.feature_importances_
+assert len(fi) > 0
+
+print('Feature importance:')
+print(*(list(zip(X.columns, fi))), sep='\n')
--- examples/tink_eval.py
+++ examples/tink_eval.py
@@ -7,6 +7,7 @@ Use OneHot-encoding representation for categorical variables
 Apply SVD compression for features with number of categories >= 10
 datapot.remove_transformer to exclude redundant transformations
 '''
+from __future__ import print_function
 
 import sys
 import bz2
@@ -16,36 +17,37 @@ from sklearn.model_selection import cross_val_score
 import datapot as dp
 from datapot.datasets import fetch_tinkoff
 
-def main():
-    fetch_tinkoff()
-    data = bz2.BZ2File('data/tink.jsonlines.bz2')
+fetch_tinkoff()
+data = bz2.BZ2File('data/tink.jsonlines.bz2')
 
-    datapot = dp.DataPot()
-    t0 = time.time()
-    datapot.fit(data)
-    print('fit time:', time.time()-t0)
-    datapot.remove_transformer('living_region', 0)
+datapot = dp.DataPot()
+t0 = time.time()
+datapot.fit(data)
+print('fit time:', time.time()-t0)
+datapot.remove_transformer('living_region', 0)
 
-    t0 = time.time()
-    df = datapot.transform(data, verbose=True)
-    print('transform time:', time.time()-t0)
+t0 = time.time()
+df = datapot.transform(data, verbose=True)
+print('transform time:', time.time()-t0)
 
-    X = df.drop(['living_region', 'open_account_flg_one_hot0', 'open_account_flg_one_hot1'], axis=1)
+X = df.drop(['living_region', 'open_account_flg_one_hot0', 'open_account_flg_one_hot1'], axis=1)
 
-    #convert '<str> 20450,00' to '<int> 20450'
-    X.credit_sum = X.credit_sum.apply(lambda x: x[:-3]).astype(int)
+#convert '<str> 20450,00' to '<int> 20450'
+X.credit_sum = X.credit_sum.apply(lambda x: x[:-3]).astype(int)
 
-    #convert '<str> 0,25689' to '<float> 0.25689'
-    X.score_shk = X.score_shk.apply(lambda x: '0.'+x[2:]).astype(float)
+#convert '<str> 0,25689' to '<float> 0.25689'
+X.score_shk = X.score_shk.apply(lambda x: '0.'+x[2:]).astype(float)
 
-    y = df['open_account_flg_one_hot1']
-    model = xgb.XGBClassifier()
-    print('Cross-val score', cross_val_score(model, X, y, cv=5))
+y = df['open_account_flg_one_hot1']
+model = xgb.XGBClassifier()
+cv_score = cross_val_score(model, X, y, cv=5)
+assert all(i > 0.5 for i in cv_score)
 
-    model.fit(X, y)
-    print('Feature importance:')
-    print(list(zip(X.columns, model.feature_importances_)))
+print('Cross-val score', cv_score)
 
-if __name__ == "__main__":
-    main()
+model.fit(X, y)
+fi = model.feature_importances_
+assert len(fi) > 0
 
+print('Feature importance:')
+print(*(list(zip(X.columns, fi))), sep='\n')
