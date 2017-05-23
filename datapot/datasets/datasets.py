f343fc02cc1f06671eb7aefc2bd46be4c15db4df
Savelyev Nikita <n.a.savelyev@gmail.com>
2017-05-23 23:43:17 +0300

    add ssa transformer evaluation

--- datapot/datasets/datasets.py
+++ datapot/datasets/datasets.py
@@ -9,7 +9,8 @@ TINKOFF_FILENAME = "tink.jsonlines.bz2"
 TINKOFF_URL = "https://www.dropbox.com/s/gwu8r2zf7ece0ws/tink.jsonlines.bz2?dl=1"
 JOB_SALARY_FILENAME = "job.jsonlines.bz2"
 JOB_SALARY_URL = "https://www.dropbox.com/s/7clzi7synp07c7d/job.jsonlines.bz2?dl=1"
-
+MALLAT_URL = "https://www.dropbox.com/s/oj64lv620y1h62o/mallat.jsonlines.bz2?dl=1"
+MALLAT_FILENAME = "mallat.jsonlines.bz2"
 DATA_HOME = 'data/'
 
 def __fetch_dataset(url, filename):
@@ -17,6 +18,9 @@ def __fetch_dataset(url, filename):
         makedirs(DATA_HOME)
     if not exists(DATA_HOME+filename):
         urlretrieve(url, DATA_HOME+filename)
+
+def __fetch_mallat():
+    __fetch_dataset(IMDB_URL, IMDB_FILENAME)
 
 def __fetch_imdb():
     __fetch_dataset(IMDB_URL, IMDB_FILENAME)
@@ -26,6 +30,22 @@ def __fetch_job_salary():
 
 def __fetch_tinkoff():
     __fetch_dataset(TINKOFF_URL, TINKOFF_FILENAME)
+
+def load_mallat(data_home=DATA_HOME):
+    """Load and return mallat dataset
+    Parameters
+    ----------
+        data_home : String
+            An arbitrary directory location to look in for mallat.jsonlines.bz2
+
+    Returns
+    -------
+        data : BZ2File
+            Bz2-compressed dataset
+    """
+    if data_home == DATA_HOME:
+        __fetch_mallat()
+    return bz2.BZ2File(DATA_HOME+MALLAT_FILENAME)
 
 def load_tinkoff(data_home=DATA_HOME):
     """Load and return tinkoff dataset
@@ -47,12 +67,10 @@ def load_imdb(data_home=DATA_HOME):
     """Load and return imdb dataset
     Parameters
     ----------
-        data_home : String
-            An arbitrary directory location to look in for imdb.jsonlines.bz2
-
+    data_home : String An arbitrary directory location to look in for imdb.jsonlines.bz2
     Returns
     -------
-        data : BZ2File
+    data : BZ2File
             Bz2-compressed dataset
     """
     if data_home == DATA_HOME:
